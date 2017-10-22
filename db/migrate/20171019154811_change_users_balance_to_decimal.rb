08346c33c1a01140831092e6c049e16d002fc5f4
rt5754 <rudithiel16@gmail.com>
2017-10-22 15:59:18 +0000

    change more decimal scales to 6

--- db/migrate/20171019154811_change_users_balance_to_decimal.rb
+++ db/migrate/20171019154811_change_users_balance_to_decimal.rb
@@ -1,5 +1,5 @@
 class ChangeUsersBalanceToDecimal < ActiveRecord::Migration[5.0]
   def change
-    change_column :users, :balance, :decimal, :precision => 10, :scale => 10, :default => 0
+    change_column :users, :balance, :decimal, :precision => 10, :scale => 6, :default => 0
   end
 end
