20fa80216de9b525685c394f559fb586f042ab9e
rt5754 <rudithiel16@gmail.com>
2017-10-22 16:10:27 +0000

    Add default array value for users

--- db/migrate/20171019162807_add_users_array_to_jackpots.rb
+++ db/migrate/20171019162807_add_users_array_to_jackpots.rb
@@ -1,5 +1,5 @@
 class AddUsersArrayToJackpots < ActiveRecord::Migration[5.0]
   def change
-    add_column :jackpots, :users, :text , array:true
+    add_column :jackpots, :users, :text , array:true, default: []
   end
 end
