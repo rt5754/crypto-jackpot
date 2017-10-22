a11c8ee7510378fafb37eb7958deeff754246812
rt5754 <rudithiel16@gmail.com>
2017-10-22 16:45:28 +0000

    Postgres error fix

--- db/migrate/20171022163951_change_ids_in_games_to_integers.rb
+++ db/migrate/20171022163951_change_ids_in_games_to_integers.rb
@@ -1,6 +1,6 @@
 class ChangeIdsInGamesToIntegers < ActiveRecord::Migration[5.0]
   def change
-    change_column :games, :user_id, :integer
-    change_column :games, :jackpot_id, :integer
+    change_column :games, :user_id, 'numeric USING CAST(user_id AS numeric)'
+    change_column :games, :jackpot_id, 'numeric USING CAST(jackpot_id AS numeric)'
   end
 end
